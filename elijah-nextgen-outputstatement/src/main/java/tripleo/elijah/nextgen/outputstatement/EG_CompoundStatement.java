c621b81459e362914e5077c4b3bbf61626e09b7f
Tripleo Nova Szi <szu@tripleo.milkyway>
2023-11-05 00:06:41 -0400

    fix: improve
    - Good commit message, check

--- elijah-nextgen-outputstatement/src/main/java/tripleo/elijah/nextgen/outputstatement/EG_CompoundStatement.java
+++ elijah-nextgen-outputstatement/src/main/java/tripleo/elijah/nextgen/outputstatement/EG_CompoundStatement.java
@@ -3,26 +3,40 @@ package tripleo.elijah.nextgen.outputstatement;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 /**
  * @author Tripleo Nova
  */
 public class EG_CompoundStatement implements EG_Statement {
-
-	private final @NotNull EG_SingleStatement beginning;
-	private final @NotNull EG_SingleStatement ending;
-	private final @NotNull EX_Explanation     explanation;
-	private final          boolean            indent;
-	private final @NotNull EG_Statement       middle;
+	private final @NotNull EG_Statement   beginning;
+	private final @NotNull EG_Statement   ending;
+	private final @NotNull EX_Explanation explanation;
+	private final          boolean        indent;
+	private final @NotNull EG_Statement   middle;
 
 	@Contract(pure = true)
-	public EG_CompoundStatement(final @NotNull EG_SingleStatement aBeginning,
-								final @NotNull EG_SingleStatement aEnding,
+	public EG_CompoundStatement(final @NotNull EG_Statement aBeginning,
+								final @NotNull EG_Statement aEnding,
 								final @NotNull EG_Statement aMiddle,
 								final boolean aIndent,
 								final @NotNull EX_Explanation aExplanation) {
 		beginning   = aBeginning;
 		ending      = aEnding;
 		middle      = aMiddle;
+		indent      = aIndent;
+		explanation = aExplanation;
+	}
+
+	public EG_CompoundStatement(final @NotNull EG_Statement aBeginning,
+								final @NotNull EG_Statement aEnding,
+								final List<EG_Statement> aList,
+								final boolean aIndent,
+								final @NotNull EX_Explanation aExplanation) {
+
+		beginning   = aBeginning;
+		ending      = aEnding;
+		middle      = new EG_SequenceStatement(new EG_Naming("<<compound>>"), aList);
 		indent      = aIndent;
 		explanation = aExplanation;
 	}
@@ -34,11 +48,7 @@ public class EG_CompoundStatement implements EG_Statement {
 
 	@Override
 	public @NotNull String getText() {
-
-		final String sb = beginning.getText() +
-				middle.getText() +
-				ending.getText();
-
+		final String sb = beginning.getText() + middle.getText() + ending.getText();
 		return sb;
 	}
 }
