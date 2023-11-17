76d28c74fc5cacdaa46901430b9f1bf21d412357
Tripleo Nova Szi <szu@tripleo.milkyway>
2023-11-17 02:01:15 -0500

    feat: Implement indentation in EG_CompoundStatement

--- elijah-nextgen-outputstatement/src/main/java/tripleo/elijah/nextgen/outputstatement/EG_CompoundStatement.java
+++ elijah-nextgen-outputstatement/src/main/java/tripleo/elijah/nextgen/outputstatement/EG_CompoundStatement.java
@@ -30,13 +30,12 @@ public class EG_CompoundStatement implements EG_Statement {
 
 	public EG_CompoundStatement(final @NotNull EG_Statement aBeginning,
 								final @NotNull EG_Statement aEnding,
-								final List<EG_Statement> aList,
+								final List<EG_Statement> aStatementList,
 								final boolean aIndent,
 								final @NotNull EX_Explanation aExplanation) {
-
 		beginning   = aBeginning;
 		ending      = aEnding;
-		middle      = new EG_SequenceStatement(new EG_Naming("<<compound>>"), aList);
+		middle      = new EG_SequenceStatement(new EG_Naming("<<compound>>"), aStatementList);
 		indent      = aIndent;
 		explanation = aExplanation;
 	}
@@ -48,7 +47,25 @@ public class EG_CompoundStatement implements EG_Statement {
 
 	@Override
 	public @NotNull String getText() {
-		final String sb = beginning.getText() + middle.getText() + ending.getText();
-		return sb;
+		final StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.append(beginning.getText());
+
+		if (indent) {
+			// TODO 11/16 write a test for this
+			final String middleText = middle.getText();
+
+			for (String s : middleText.split("\n")) {
+				stringBuilder.append("\t");
+				stringBuilder.append(s);
+				stringBuilder.append("\n");
+			}
+		} else {
+			stringBuilder.append(middle.getText());
+		}
+
+		stringBuilder.append(ending.getText());
+
+		return stringBuilder.toString();
 	}
 }
