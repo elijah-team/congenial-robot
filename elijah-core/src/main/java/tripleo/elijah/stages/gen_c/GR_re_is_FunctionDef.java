ba7378cc6d45455855aa4e2913ea636eb758ad19
tripleo <tripleo@tripleo.tripleo>
2024-02-06 02:50:55 -0500

    Documentation

--- elijah-core/src/main/java/tripleo/elijah/stages/gen_c/GR_re_is_FunctionDef.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/gen_c/GR_re_is_FunctionDef.java
@@ -1,68 +1,51 @@
 package tripleo.elijah.stages.gen_c;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+//import org.eclipse.xtend.lib.annotations.EqualsHashCode;
+
+import org.eclipse.xtend.lib.annotations.EqualsHashCode;
 import org.jdeferred2.DoneCallback;
 import tripleo.elijah.Eventual;
+
 import tripleo.elijah.lang.i.OS_Element;
+
 import tripleo.elijah.stages.gen_fn.EvaClass;
 import tripleo.elijah.stages.gen_fn.EvaNode;
+
 import tripleo.elijah.stages.gen_fn.IdentTableEntry;
 import tripleo.elijah.stages.gen_fn.ProcTableEntry;
 
-import java.util.Objects;
-
+@EqualsAndHashCode
+// @EqualsHashCode
+//  https://eclipse.dev/Xtext/xtend/documentation/204_activeannotations.html#data-annotation
 public final class GR_re_is_FunctionDef implements GRRR, GR_EvaNodeAble {
+	@Getter
 	private final ProcTableEntry  pte;
-	private final EvaClass        cheat;
+	@Getter
+	private final EvaClass        cheatClass;
+	@Getter
 	private final IdentTableEntry ite;
+	@Getter
 	private final CRI_Ident       cri_ident;
 	private final OS_Element      repo_element;
 	private final GI_FunctionDef  gi_item;
 
-	public GR_re_is_FunctionDef(ProcTableEntry aPte, EvaClass a_cheat, IdentTableEntry aIte, final CRI_Ident aCRIIdent, GI_FunctionDef aGiItem) {
-		this.pte     = aPte;
-		this.cheat   = a_cheat;
-		this.ite     = aIte;
-		cri_ident    = aCRIIdent;
-		repo_element = ite.getResolvedElement();
-		gi_item      = aGiItem;
-	}
-
-	public ProcTableEntry aPte() {
-		return pte;
-	}
-
-	public EvaClass a_cheat() {
-		return cheat;
-	}
-
-	public IdentTableEntry aIte() {
-		return ite;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == this) return true;
-		if (obj == null || obj.getClass() != this.getClass()) return false;
-		var that = (GR_re_is_FunctionDef) obj;
-		return Objects.equals(this.pte, that.pte) &&
-				Objects.equals(this.cheat, that.cheat) &&
-				Objects.equals(this.ite, that.ite);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(pte, cheat, ite);
-	}
-
-	@Override
-	public String toString() {
-		return "GR_re_is_FunctionDef[" +
-				"aPte=" + pte + ", " +
-				"a_cheat=" + cheat + ", " +
-				"aIte=" + ite + ']';
-	}
-
 	private final Eventual<EvaNode> resolvedP = new Eventual<>();
+
+	// TODO too many params make jack feel like a cheater
+	public GR_re_is_FunctionDef(final ProcTableEntry aPte,
+								final EvaClass a_cheat,
+								final IdentTableEntry aIte,
+								final CRI_Ident aCRIIdent,
+								final GI_FunctionDef aGiItem) {
+		this.pte          = aPte;
+		this.cheatClass   = a_cheat;
+		this.ite          = aIte;
+		this.cri_ident    = aCRIIdent;
+		this.repo_element = ite.getResolvedElement();
+		this.gi_item      = aGiItem;
+	}
 
 	@Override
 	public void onResolve(final DoneCallback<EvaNode> cb) {
@@ -70,13 +53,10 @@ public final class GR_re_is_FunctionDef implements GRRR, GR_EvaNodeAble {
 		resolvedP.then(cb);
 	}
 
-	public CRI_Ident getCri_ident() {
-		return cri_ident;
-	}
-
 	@Override
 	public void reverseResolving(final Object aObject) {
-		//assert
-		this.gi_item._re_is_FunctionDef(pte, cheat, ite, resolvedP);
+		assert aObject == this;
+		assert aObject instanceof EqualsHashCode; // TODO trigger
+		this.gi_item._re_is_FunctionDef(pte, cheatClass, ite, resolvedP);
 	}
 }
