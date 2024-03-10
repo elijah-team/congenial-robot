f6d68540f85f76250e99dfa98ffe46c3720716dd
Tripleo <novixf@tripleo.nonexistent>
2024-03-09 23:06:34 -0500

    2024-03-09 023a: IEvaClass I

--- elijah-core/src/main/java/tripleo/elijah/comp/i/IPipelineAccess.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/i/IPipelineAccess.java
@@ -17,7 +17,7 @@ import tripleo.elijah.nextgen.output.NG_OutputItem;
 import tripleo.elijah.stages.deduce.DeducePhase;
 import tripleo.elijah.stages.gen_c.GenerateC;
 import tripleo.elijah.stages.gen_fn.BaseEvaFunction;
-import tripleo.elijah.stages.gen_fn.EvaClass;
+import tripleo.elijah.stages.gen_fn.IEvaClass;
 import tripleo.elijah.stages.gen_fn.EvaNamespace;
 import tripleo.elijah.stages.gen_fn.EvaNode;
 import tripleo.elijah.stages.gen_generic.pipeline_impl.GenerateResultSink;
@@ -28,51 +28,21 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public interface IPipelineAccess {
-	void _setAccessBus(AccessBus ab);
-
-	void addFunctionStatement(FunctionStatement aFunctionStatement);
-
 	void addLog(ElLog aLOG);
-
 	void addOutput(NG_OutputItem aO);
 
-	AccessBus getAccessBus();
-
-	Compilation getCompilation();
-
-	CompilationEnclosure getCompilationEnclosure();
-
-	GenerateResultSink getGenerateResultSink();
-
-	//List<NG_OutputItem> getOutputs();
-
-	@NotNull Eventual<PipelineLogic> getPipelineLogicPromise();
-
-	WritePipeline getWritePipeline();
-
-	void notate(Provenance provenance, GN_Notable aNotable);
-
+	void setNodeList(List<EvaNode> aEvaNodeList);
 	void registerNodeList(DoneCallback<List<EvaNode>> done);
 
-	void setEvaPipeline(@NotNull EvaPipeline agp);
-
-	void setGenerateResultSink(GenerateResultSink aGenerateResultSink);
-
-	void setNodeList(List<EvaNode> aEvaNodeList);
-
-	void setWritePipeline(WritePipeline aWritePipeline);
-
-	void activeFunction(BaseEvaFunction aEvaFunction);
-
-	void activeClass(EvaClass aEvaClass);
+	void activeClass(IEvaClass aEvaClass);
+	List<IEvaClass> getActiveClasses();
 
 	void activeNamespace(EvaNamespace aEvaNamespace);
-
 	List<EvaNamespace> getActiveNamespaces();
 
 	List<BaseEvaFunction> getActiveFunctions();
-
-	List<EvaClass> getActiveClasses();
+	void activeFunction(BaseEvaFunction aEvaFunction);
+	void addFunctionStatement(FunctionStatement aFunctionStatement);
 
 	void _send_GeneratedClass(EvaNode aClass);
 
@@ -80,15 +50,30 @@ public interface IPipelineAccess {
 
 	void install_notate(Provenance aProvenance, Class<? extends GN_Notable> aRunClass, Class<? extends GN_Env> aEnvClass);
 
+	void notate(Provenance provenance, GN_Notable aNotable);
 	void notate(Provenance aProvenance, GN_Env aPlRun2);
-
-	void resolvePipelinePromise(PipelineLogic aPipelineLogic);
-
 	DeducePhase getDeducePhase();
 
 	EIT_ModuleList getModuleList();
 
 	void _ModuleList_add(OS_Module aM);
 
-	Eventual<EvaPipeline> getEvaPipelinePromise();
+
+	void resolvePipelinePromise(PipelineLogic aPipelineLogic);
+
+	void setWritePipeline(WritePipeline aWritePipeline);
+	void setEvaPipeline(@NotNull EvaPipeline agp);
+	void setGenerateResultSink(GenerateResultSink aGenerateResultSink);
+
+	void _setAccessBus(AccessBus ab);
+
+	@NotNull Eventual<EvaPipeline> getEvaPipelinePromise();
+	@NotNull Eventual<PipelineLogic> getPipelineLogicPromise();
+
+	AccessBus getAccessBus();
+	Compilation getCompilation();
+	CompilationEnclosure getCompilationEnclosure();
+	GenerateResultSink getGenerateResultSink();
+	WritePipeline getWritePipeline();
+	//List<NG_OutputItem> getOutputs();
 }
