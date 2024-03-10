5d4632e228d7351a4bbc599b0b04740e7019bb61
Tripleo <novixf@tripleo.nonexistent>
2024-03-09 23:11:43 -0500

    2024-03-09 023a: IEvaClass X

--- elijah-core/src/main/java/tripleo/elijah_congenial/deduce/t/TEvaClass.java
+++ elijah-core/src/main/java/tripleo/elijah_congenial/deduce/t/TEvaClass.java
@@ -1,13 +1,13 @@
 package tripleo.elijah_congenial.deduce.t;
 
 import tripleo.elijah.lang.i.OS_Module;
-import tripleo.elijah.stages.gen_fn.EvaClass;
 import tripleo.elijah.stages.gen_fn.EvaNode;
+import tripleo.elijah.stages.gen_fn.IEvaClass;
 
 public class TEvaClass implements EvaNode {
 	private final EvaNode aClass;
 
-	public TEvaClass(final EvaClass aClass) {
+	public TEvaClass(final IEvaClass aClass) {
 		this.aClass = aClass;
 	}
 
