2c0466d9531c2eea3f919eded743fddf8ac21ac3
tripleo <tripleo@tripleo.milkyway>
2025-11-18 04:53:10 -0500

    ExceptionDiagnostic: Throwable constructor

--- elijah-congenial-core/src/main/java/tripleo/elijah_durable_congenial/comp/diagnostic/ExceptionDiagnostic.java
+++ elijah-congenial-core/src/main/java/tripleo/elijah_durable_congenial/comp/diagnostic/ExceptionDiagnostic.java
@@ -8,9 +8,12 @@ import java.io.PrintStream;
 import java.util.List;
 
 public class ExceptionDiagnostic implements Diagnostic {
-	private final Exception e;
+	private final Throwable e;
 
 	public ExceptionDiagnostic(final Exception aE) {
+		e = aE;
+	}
+	public ExceptionDiagnostic(final Throwable aE) {
 		e = aE;
 	}
 
